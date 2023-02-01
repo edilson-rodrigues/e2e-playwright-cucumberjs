aeb97f4e03fa058c21e94c35179f964fcabb5e39
Edilson Rodrigues <edarodrigs@gmail.com>
2023-01-31 21:58:47 -0300

    build(setup): config playwright and cucumberjs

---
+++ cucumber.js
@@ -0,0 +1,11 @@
+module.exports = {
+  default: {
+    parallel: 2,
+    formt: ['progress', 'html:cucumber-report.html'],
+    publishQuiet: true,
+    retry: 1,
+    requireModule: ['ts-node/register'],
+    require: ['./tests/steps/*.steps.ts'],
+    paths: ['./tests/features'],
+  }
+}
